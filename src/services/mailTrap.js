86202b99ef08065f91d4e9f0d2be9f0797cbada8
Aryan Qayum <aryan.qayum666@gmail.com>
2026-06-16 20:25:38 +0530

    not getting emai

--- src/services/mailTrap.js
+++ src/services/mailTrap.js
@@ -1,15 +1,21 @@
-const {Resend} = require('resend')
+const { Resend } = require('resend')
 
-async function sendMail(user_email,generated_otp) {
-  const resend = new Resend('re_3e4ma7wm_CuUrHwB2Gkm7w6q5bhpWFh8L')
-  await resend.emails.send({
-  from: 'onboarding@resend.dev',
-  to: user_email,
-  subject: 'OTP :',
-  html: generated_otp  
-});
+async function sendMail(user_email, generated_otp) {
+  try {
+    const resend = new Resend(process.env.RESEND_API_KEY)
 
-console.log("Email Sent:");
+    const data = await resend.emails.send({
+      from: 'onboarding@resend.dev',
+      to: user_email,
+      subject: 'OTP',
+      html: `<h1>${generated_otp}</h1>`
+    })
+
+    console.log("RESEND RESPONSE:", data)
+
+  } catch (err) {
+    console.error("RESEND ERROR:", err)
+  }
 }
 
-module.exports  = sendMail
+module.exports = sendMail
