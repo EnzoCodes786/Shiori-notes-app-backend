afe8230d221d8317282bedf261c23780cb31ae19
Aryan Qayum <aryan.qayum666@gmail.com>
2026-06-16 20:19:36 +0530

    Updating key

--- src/services/mailTrap.js
+++ src/services/mailTrap.js
@@ -1,7 +1,7 @@
 const {Resend} = require('resend')
 
 async function sendMail(user_email,generated_otp) {
-  const resend = new Resend(process.env.RESEND_API_KEY)
+  const resend = new Resend('re_3e4ma7wm_CuUrHwB2Gkm7w6q5bhpWFh8L')
   await resend.emails.send({
   from: 'onboarding@resend.dev',
   to: user_email,
